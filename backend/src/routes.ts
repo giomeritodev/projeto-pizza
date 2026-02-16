119591739e12b773acef6fc8e268fe0ea95bff9e
Giomerito Alves de Souza <giomerito.dev@gmail.com>
2026-02-15 21:21:41 -0300

    Detalhes do usuario

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,11 +3,13 @@ import { CreateUserController } from "./controllers/user/CreateUserController"
 import { validateSchema } from "./middlewares/validateSchema"
 import { createUserSchema, authUserSchema } from "./schemas/userSchema"
 import { AuthUserController } from "./controllers/user/AuthUserController"
+import { DetailUserController } from "./controllers/user/DetailUserController"
 
 const router = Router()
 
 //Rotas de Usuários
 router.post("/users", validateSchema(createUserSchema), new CreateUserController().handle)
 router.post("/session", validateSchema(authUserSchema), new AuthUserController().handle)
+router.post("/me", new DetailUserController().handle)
 
 export { router }
---
+++ backend/src/services/user/DetailUserService.ts
@@ -0,0 +1,32 @@
+import { prisma } from "../../prisma"
+
+
+
+class DetailUserService {
+    async execute(user_id: number) {
+        try {
+            const user = await prisma.user.findFirst({
+                where: {
+                    id: user_id
+                },
+                select: {
+                    id: true,
+                    name: true,
+                    email: true,
+                    role: true,
+                    createdAt: true,
+                }
+            })
+            if (!user) {
+                throw new Error("Usuário não encontrado")
+            }
+            return user;
+
+        } catch (error) {
+            throw new Error("Usuário não encontrado.")
+        }
+
+    }
+}
+
+export { DetailUserService }
